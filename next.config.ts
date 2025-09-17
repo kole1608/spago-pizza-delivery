e87dab6aefcf6cb9a9bec0d11d54ffae930c50f7
kole1608 <slobodan90kostic@gmail.com>
2025-09-17 23:25:07 +0200

    Fix NextAuth cookies context and JSON response errors

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
   output: 'standalone',
   experimental: {
     serverActions: {
@@ -16,6 +19,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-};
+});
 
 export default nextConfig;
